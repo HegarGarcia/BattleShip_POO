82f9185732cbfd4e34e6344fe7d1ee8589c72a3f
Hegar Garcia <hegargarcia@gmail.com>
2018-04-23 19:38:46 -0500

    Changing constructor

--- class_board.js
+++ class_board.js
@@ -1,7 +1,7 @@
 export default class Board {
     constructor() {
         let props = {length: 10};
-        let row = Array.from(props);
+        let row = Array.from(props).fill({boat: false, shot: false});
         this.board = Array.from(props).fill(row);
     }
 }
