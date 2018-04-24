cb97913d280d5093842abd28fdde5e08f9078f23
Hegar Garcia <hegargarcia@gmail.com>
2018-04-23 19:39:58 -0500

    Minor changes and adding shot method

--- class_player.js
+++ class_player.js
@@ -1,6 +1,33 @@
-import Board from './class_board';
-import Ship from './class_ship';
+import Board from './class_board.js';
+import Boat from './class_boat.js';
 
-export default class Player {
-    constructor() {}
+class Player {
+    constructor() {
+        this.boats = {
+            'carrier': new Boat({len: 5, name: 'carrier'}),
+            'battleship': new Boat({len: 4, name: 'battleship'}),
+            'cruiser': new Boat({len: 3, name: 'cruiser'}),
+            'submarine': new Boat({len: 3, name: 'submarine'}),
+            'destroyer': new Boat({len: 2, name: 'destroyer'})
+        };
+        
+        this.opponentBoard = new Board();
+        this._board = new Board();
+    }
+
+    shot({x, y}, player) {
+        if (!(player instanceof Player)) throw new Error('No es jugador válido');
+
+        let spot = player._board.board[x][y];
+
+        if (spot.boat !== false && spot.shot === false) {
+            player.boats[spot.boat].health--;
+            spot.shot = true;
+        }
+    }
 }
+
+let global = window || global;
+
+global.Player = Player;
+global.Board = Board;
