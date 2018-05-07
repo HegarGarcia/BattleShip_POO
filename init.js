95d14d8743912881205fe2511e4ddd1a7d8fe6b8
Hegar Garcia <hegargarcia@gmail.com>
2018-05-07 18:22:58 -0500

    Test class

--- init.js
+++ init.js
@@ -8,4 +8,10 @@
     p1.putBoat("cruiser", {x: 0, y: 2, direction: {x: 1}});
     p1.putBoat("submarine", {x: 0, y: 3, direction: {x: 1}});
     p1.putBoat("destroyer", {x: 0, y: 4, direction: {x: 1}});
+
+    p1.shot("carrier", {x: 3, y: 2, direction: {y: -1}});
+    p1.shot("battleship", {x: 3, y: 7, direction: {x: -1}});
+    p1.shot("submarine", {x: 4, y: 6, direction: {x: 1}});
+    p1.shot("destroyer", {x: 6, y: 4, direction: {y: -1}});
+    p1.shot("cruiser", {x: 6, y: 3, direction: {x: 1}});
 })()
